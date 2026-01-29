bc2c00c5dc1442c918bae6e40a45fd526659552f
Abdellah Hariti <haritiabdellah@gmail.com>
2026-01-29 12:17:56 +0100

    improve undefined behavior shifts exercise

--- exercises/28_undefined_behavior/03_shift_ub.c
+++ exercises/28_undefined_behavior/03_shift_ub.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include "clings.h"
 
@@ -14,6 +15,9 @@ int safe_shift(int value, int shift) {
 int main(void) {
     check_int_msg(safe_shift(1, 3), 8, "1 << 3 should be 8");
     check_int_msg(safe_shift(1, -1), 0, "Negative shifts should be rejected");
+    check_int_msg(safe_shift(1, 31), 0, "bigger than bit width shifts should be rejected");
+    check_int_msg(safe_shift(1, 32), 0, "bigger than bit width shifts should be rejected");
+    check_int_msg(safe_shift(1, 33), 0, "bigger than bit width shifts should be rejected");
 
     return 0;
 }
